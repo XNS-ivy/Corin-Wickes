6e68b899a55964d420c916632b4895573768e048
XNS-ivy <ayramusic.nightcore@gmail.com>
2024-11-05 10:07:57 +0700

    new update fix some issue

--- libs/whatsapp.js
+++ libs/whatsapp.js
@@ -9,7 +9,7 @@ async function corinSocket() {
         auth: state,
         emitOwnEvents: false,
         logger: pino({ level: 'silent' }),
-        browser: ['Takanashi Hoshino', 'Chrome', '1.0.0'],
+        browser: ['Corin Wickes', 'Chrome', '1.0.0'],
     });
     corin.ev.on("creds.update", saveCreds)
     corin.ev.on('connection.update', update => {
@@ -18,7 +18,7 @@ async function corinSocket() {
             const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
             console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
             if (shouldReconnect) {
-                hoshinoSocket()
+                corinSocket()
             }
         } else if (connection === 'open') {
             console.log('opened connection')
